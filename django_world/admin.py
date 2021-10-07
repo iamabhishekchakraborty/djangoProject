af2bdcd938d4a56e03a17b7e3efe7854a0b16d36
Abhishek <abhis64673@gmail.com>
2021-10-07 17:20:35 +0530

    app deployment to heroku, add a model to admin site

--- django_world/admin.py
+++ django_world/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Succession,Succession_Seasons,Succession_Casts,Succession_Season_Episodes
 
 # Register your models here.
-# admin.site.register(Succession)                # The model Succession is abstract so it can't be registered with admin
+# If a model is abstract it can't be registered with admin
+admin.site.register(Succession)
 admin.site.register(Succession_Seasons)
 admin.site.register(Succession_Casts)
 admin.site.register(Succession_Season_Episodes)
